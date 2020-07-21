f34f7ea95237244f4e38f7a4a98b4ae3cccc3448
Nawab Khairuzzaman Mohammad Kibria <44573643+nawab69@users.noreply.github.com>
2020-07-21 19:56:03 +0600

    Updates

---
+++ precache-manifest.bdba1fa89c238b97897d3e20a39287b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef332ba11744b9770ade0b8cdcb5cd37",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16c3f3c8f3a7bb7e2df1",
+    "url": "/monsters-rolodex/static/css/main.d5f57c55.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c3f3c8f3a7bb7e2df1",
+    "url": "/monsters-rolodex/static/js/main.679ed4f2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
