9f5c77f118e036af46d9a196d0d43d4386130b04
Nawab Khairuzzaman Mohammad Kibria <44573643+nawab69@users.noreply.github.com>
2020-07-21 19:33:32 +0600

    deploy first react project

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './card-list.styles.css'
+import { Card } from '../card/card.component'
+
+
+export const CardList = props => {
+    return <div class="card-list">
+    
+    {
+        props.monsters.map(
+
+            monster => <Card key={monster.id} monster={monster}/>
+        
+        )
+    }
+    </div>
+}
